2b32e610e3d7c0651d5c050f7e1cb76009bbfc9a
Replit user <>
2022-09-12 01:34:13 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -103,12 +103,16 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-  Pair* p;
-  for (int i=0;i<map->capacity;i++){
-    if(p!=NULL){
-      return p->value;
+  int i;
+  Pair* p = (Pair *)malloc(sizeof(Pair));
+  for(i=0; i<map->capacity; i++){
+    if(map->buckets[i] != NULL && map->buckets[i]->key != NULL){
+      p=map->buckets[i];
+      break;
     }
   }
+  map->current= i;
+return p;
 }
 
 Pair * nextMap(HashMap * map) {
